0fd7a76346b8d4b415041719650d9b806878d9e9
sapro313 <139606757+sapro313@users.noreply.github.com>
2025-05-31 09:25:35 +0530

    Create back_space_string_conparte.cpp

---
+++ back_space_string_conparte.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+      string finder(string s){
+        string ans1="";
+        int x1=0;
+       for(int i=s.size()-1;i>=0;i--){
+           if(s[i]=='#'){
+           x1++;
+              }
+              else{    
+  
+  }
+
+        
+    
+};
