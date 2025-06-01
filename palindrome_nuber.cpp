196d70fe618a787680d40c88586250cebffe02b9
sapro313 <139606757+sapro313@users.noreply.github.com>
2025-06-01 17:37:35 +0530

    Create palindrome_nuber.cpp

---
+++ palindrome_nuber.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+bool is_palin(string ans){
+    int a=0,b=ans.size()-1;
+    while(a<b){
+      if(ans[a]!=ans[b]){
+        return false;
+      }
+
+
+…
+
+         }
+         return is_palin(str);
+
+         
+    }
+};
