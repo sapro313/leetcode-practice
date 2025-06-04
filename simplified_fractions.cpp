2b7837629ed894cf965b86517d6c8d6478e31088
sapro313 <139606757+sapro313@users.noreply.github.com>
2025-06-04 10:52:18 +0530

    Create simplified_fractions.cpp

---
+++ simplified_fractions.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+vector<string>ans;
+    vector<string> simplifiedFractions(int n) {
+        set<float>s1;
+
+        for(int i=1;i<n;i++){
+
+            for(int j=i+1;j<=n;j++){
+                   
+…        return ans;
+        
+    }
+};
