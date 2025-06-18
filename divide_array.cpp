6c54abab6ce466211b2a121d02c6d707bace6307
sapro313 <139606757+sapro313@users.noreply.github.com>
2025-06-18 12:55:47 +0530

    Create divide_array.cpp

---
+++ divide_array.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<vector<int>> divideArray(vector<int>& nums, int k) {
+        sort(nums.begin(),nums.end());
+        vector<vector<int>>v1;
+        for(int i=0;i<nums.size()-2;i+=3){
+            if(nums[i+2]-nums[i]>k){
+                return {};
+               
+            }
+
+           v1.push_back({nums[i],nums[i+1],nums[i+2]});
+           
+
+
+
+
+        }
+   
+   
+
+   return v1;
+
+
+    }
+};
