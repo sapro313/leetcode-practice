5df7b4f955c7e6fd58b3f0acd98a5937b5eda7ee
sapro313 <139606757+sapro313@users.noreply.github.com>
2025-06-26 19:47:55 +0530

    Create number of recent calls.cpp

---
+++ number of recent calls.cpp
@@ -0,0 +1,31 @@
+class RecentCounter {
+public:
+    queue<int>q;
+    RecentCounter() {
+        while(!q.empty()){
+            q.pop();
+        }
+    }
+    
+    int ping(int t) {
+        
+        q.push(t);
+        while(!q.empty()){
+            if(t-3000>q.front()){
+               q.pop();
+            }
+            else{
+                break;
+            }
+         
+         
+        }
+        return q.size();
+    }
+};
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * RecentCounter* obj = new RecentCounter();
+ * int param_1 = obj->ping(t);
+ */
